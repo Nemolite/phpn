d4eab35cd5de33fa7e98ce77df4c6af8f1d103d2
Nemolite <g25092011@mail.ru>
2022-03-14 09:49:18 +0300

    s3

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
-echo "111";
+/**
+ * ООП
+ */
+
+ class MyClass {
+    /**
+     * Публичное свойство класса
+     *
+     * @var string
+     */ 
+    public $myvar = 'Значение по умолчанию';
+
+    /**
+     * Публичный метод
+     *
+     * @return void
+     */
+    public function show() {
+        echo $this->myvar;
+    }
+ }     
+    
+ /**
+  * Создание объекта, экземпляра класса
+  */
+$myobj = new MyClass();
+$myobj->show();
+echo "<br>";    
+echo "===========";
+echo "<br>"; 
+echo $myobj->myvar;
 ?>
